df9ae85bdbe0be7c72eb0660fa5d336f0f5a9cee
Andrei Valentin Constantin Stefan <andreivc7121@gmail.com>
2024-03-13 11:00:42 +0100

    It doesn't work :(

--- src/sisa.vhd
+++ src/sisa.vhd
@@ -18,29 +18,45 @@ ENTITY sisa IS
 END sisa;
 
 ARCHITECTURE Structure OF sisa IS
+    SIGNAL data_rd: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL data_wr: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL data_addr: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL data_byte: STD_LOGIC;
+    SIGNAL we: STD_LOGIC;
+    SIGNAL clock4: STD_LOGIC;
 BEGIN
 
-pro0: proc
-    PORT MAP(   clk => CLOCK_50,
-                boot => SW(9),
-                datard_m => datard_m,
-                addr_m => addr_m,
-                data_wr => data_wr,
-                wr_m => wr_m,
-                word_byte => word_byte);
+    pro0: proc
+        PORT MAP(   clk => clock4,
+                    boot => SW(9),
+                    datard_m => data_rd,
+                    addr_m => data_addr,
+                    data_wr => data_wr,
+                    wr_m => we,
+                    word_byte => data_byte);
 
-mem0: MemoryController
-PORT MAP (  CLOCK_50  => CLOCK_50, 
-            addr      => addr, 
-            wr_data   => wr_data, 
-            rd_data   => rd_data, 
-            we        => we, 
-            byte_m    => byte_m, 
-            SRAM_ADDR => SRAM_ADDR,
-            SRAM_DQ   => SRAM_DQ, 
-            SRAM_UB_N => SRAM_UB_N,
-            SRAM_LB_N => SRAM_LB_N,
-            SRAM_CE_N => SRAM_CE_N,
-            SRAM_OE_N => SRAM_OE_N,
-            SRAM_WE_N => SRAM_WE_N);
+    mem0: MemoryController
+    PORT MAP (  CLOCK_50  => CLOCK_50, 
+                addr      => data_addr, 
+                wr_data   => data_wr, 
+                rd_data   => data_rd, 
+                we        => we, 
+                byte_m    => data_byte, 
+                SRAM_ADDR => SRAM_ADDR,
+                SRAM_DQ   => SRAM_DQ, 
+                SRAM_UB_N => SRAM_UB_N,
+                SRAM_LB_N => SRAM_LB_N,
+                SRAM_CE_N => SRAM_CE_N,
+                SRAM_OE_N => SRAM_OE_N,
+                SRAM_WE_N => SRAM_WE_N);
+
+    PROCESS (CLOCK_50)
+        VARIABLE clock4_counter: unsigned(1 DOWNTO 0) := to_unsigned(2, 2);
+    BEGIN
+        IF rising_edge(CLOCK_50) THEN
+            clock4_counter := clock4_counter + 1;
+            clock4 <= clock4_counter(1);
+        END IF;
+    END PROCESS;
+
 END Structure;
